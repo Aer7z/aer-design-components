dc88bc4585c8905912520e4c396c102db556b481
Aer7z <570991904@qq.com>
2024-05-30 21:31:47 +0800

    实现加载图标的旋转

--- src/components/icon/icon-loading.vue
+++ src/components/icon/icon-loading.vue
@@ -1,11 +1,11 @@
 <template>
   <!-- <input type="checkbox" v-model="checked" @change="handleChange" />
  -->
-
-  <svg width="20px" height="20px">
-    <use href="/src/assets/loading.svg#loading-icon"></use>
-    <!-- <use href="../../assets/loading.svg#loading-icon"></use> -->
-  </svg>
+  <img src="/src/assets/loading.svg" width="20px" height="20px"/>
+  <!-- <svg width="20px" height="20px">
+    <use href="/src/assets/loading.svg#loading-icon"></use> -->
+  <!-- <use href="../../assets/loading.svg#loading-icon"></use> -->
+  <!-- </svg> -->
 </template>
 
 <script setup lang="ts">
